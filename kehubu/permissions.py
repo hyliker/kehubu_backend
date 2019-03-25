e9956404236aab491fe1e57304ee9c47634f2f8e
Hyliker <hyliker@themanxgroup.tw>
2019-03-25 15:49:08 +0800

    Add kehubu permissions

---
+++ kehubu/permissions.py
@@ -0,0 +1,54 @@
+from rest_framework import permissions
+
+
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
+class IsOwner(permissions.BasePermission):
+    owner_field = 'owner'
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(view, "owner_field"):
+            owner = getattr(obj, view.owner_field)
+        else:
+            owner = getattr(obj, self.owner_field)
+        return owner == request.user
+
+class IsGroupCreator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.group.creator
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    owner_field = 'owner'
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(view, "owner_field"):
+            owner = getattr(obj, view.owner_field)
+        else:
+            owner = getattr(obj, self.owner_field)
+        return owner == request.user
+
+
+class IsGroupCreatorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.group.creator
