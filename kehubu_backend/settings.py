acac7ab1ba5a2529f175c8339a3b7ee3187f42a0
hyliker <hyliker@gmail.com>
2019-04-03 16:40:45 +0800

    Add /api/ url prefix for django request

--- kehubu_backend/settings.py
+++ kehubu_backend/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 
 REST_FRAMEWORK = {
--- kehubu_backend/urls.py
+++ kehubu_backend/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('kehubu/', include('kehubu.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/kehubu/', include('kehubu.urls')),
+    path('api/auth/', include('rest_framework.urls')),
+    path('api/accounts/', include('allauth.urls')),
 ]
