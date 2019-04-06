6e2e14d5a98bf7008f1668caae392674bf493592
hyliker <hyliker@gmail.com>
2019-04-06 15:43:44 +0800

    Revise KehubuSocialAccountAdapter
    update profille with weixin social user info

--- kehubu/adapters.py
+++ kehubu/adapters.py
@@ -10,3 +10,9 @@ class KehubuAccountAdapter(DefaultAccountAdapter):
 class KehubuSocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request, sociallogin):
         return True
+
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        provider = sociallogin.account.provider
+        user.kehubu_profile.update_by_socialaccount(provider)
+        return user
