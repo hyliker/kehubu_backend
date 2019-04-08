baccf69453365586328a6bde94ac84ccb331f542
Hyliker <hyliker@themanxgroup.tw>
2019-04-08 14:29:27 +0800

    Add settings.CHANNEL_LAYERS

--- kehubu_backend/settings.py
+++ kehubu_backend/settings.py
@@ -124,7 +124,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+# channels
 ASGI_APPLICATION = "kehubu_backend.routing.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
