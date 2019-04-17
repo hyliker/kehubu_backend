245d5783c8a2234fb1fdfc810712e8483af276f4
Hyliker <hyliker@themanxgroup.tw>
2019-04-17 12:11:17 +0800

    add forum admins

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Category, Topic, Post, Attachment
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(MPTTModelAdmin):
+    pass
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'created', 'modified')
+    list_filter = ('created', 'modified')
+    search_fields = ('title', )
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('topic', 'summary', 'created', 'modified')
+    list_filter = ('created', 'modified')
+    search_fields = ('content', )
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('group', 'creator', 'file', 'created')
+    list_filter = ('created', 'modified')
+    search_fields = ('file', )
